6dc072d71334fa490f048f00d327dfcd77f8e2ae
Arijit15091999 <arijitkarmakar056@gmail.com>
2024-02-12 17:13:35 +0530

    done

---
+++ LinkedList/singly_linkedList/Leetcode141.java
@@ -0,0 +1,16 @@
+package singly_linkedList;
+
+public class Leetcode141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while (fast != null && fast.next != null) {            
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+}
