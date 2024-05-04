ad9e214e8bd5d253f37156fc8044ff5d5e8ff0ea
Arijit15091999 <arijitkarmakar056@gmail.com>
2024-05-04 09:23:44 +0530

    done

--- adhoc/Leetcode165.java
+++ adhoc/Leetcode165.java
@@ -1,6 +1,6 @@
 public class Leetcode165 {
     public static void main(String[] args) {
-        System.out.println(compareVersion("1.0", "1.0.0"));
+        System.out.println(compareVersionOptimal("1.0", "1.0.0"));
     }
     public static int compareVersion(String version1, String version2) {
         int res = 0;
@@ -9,14 +9,11 @@ public class Leetcode165 {
 
         int size = Math.min(v1.length, v2.length);
 
-//        System.out.println(size);
 
         for(int i = 0; i < size; i++) {
             int num1 = Integer.parseInt(v1[i]);
             int num2 = Integer.parseInt(v2[i]);
 
-//            System.out.println(num1);
-//            System.out.println(num2);
 
             if (num1 > num2) {
                 return 1;
@@ -40,5 +37,27 @@ public class Leetcode165 {
         }
 
         return res;
+    }
+
+    public static int compareVersionOptimal(String version1, String version2) {
+        int i = 0, j = 0;
+
+        while(i < version1.length() || j < version2.length()) {
+            int num1 = 0, num2 = 0;
+            while(i < version1.length() && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + version1.charAt(i++) - '0';
+            }
+
+            while(j < version2.length() && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + version2.charAt(j++) - '0';
+            }
+
+
+            if(num1 > num2) return 1;
+            else if(num1 < num2) return -1;
+            i++;
+            j++;
+         }
+        return 0;
     }
 }
