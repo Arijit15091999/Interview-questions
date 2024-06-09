a519ca4bb696176b5c84901162c17d48f2951c0b
Arijit15091999 <arijitkarmakar056@gmail.com>
2024-06-10 00:24:03 +0530

    updated single elemnet.jsvs

--- Binary_Search/SingleElement.java
+++ Binary_Search/SingleElement.java
@@ -4,42 +4,35 @@ public class SingleElement {
         System.out.println(singleNonDuplicate(arr));
     }
     public static int singleNonDuplicate(int[] nums) {
-        if(nums.length == 1) {
+        int n = nums.length;
+        
+        if (n == 1 || nums[0] != nums[1]) {
             return nums[0];
         }
-
-        if(nums[0] != nums[1]) {
-            return nums[0];
+        if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
         }
+        int start = 1, end = n - 2;
 
-        if(nums[nums.length -2] != nums[nums.length - 1]){
-            return nums[nums.length - 1];
-        }
-
-        int start = 1, end = nums.length - 2;
-
-        while(start <= end) {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
-
-            if(nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
-                return nums[mid];
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return mid;
             }
-
-            if(mid % 2 == 0) {
-                if(nums[mid] == nums[mid + 1]) {
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
                     start = mid + 1;
-                }else{
+                } else {
                     end = mid - 1;
                 }
-            }else{
-                if(nums[mid] == nums[mid - 1]) {
-                    start = mid + 1;
-                }else{
+            } else {
+                if (nums[mid] == nums[mid + 1]) {
                     end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
             }
         }
-
         return -1;
     }
 }
