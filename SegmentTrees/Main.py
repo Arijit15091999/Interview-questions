0590f1ea0a2b54a2e829e5130285dc38de310696
Arijit15091999 <155151121+Arijit15091999@users.noreply.github.com>
2025-12-07 06:11:18 +0000

    min max tree added

--- SegmentTrees/Main.py
+++ SegmentTrees/Main.py
@@ -3,9 +3,5 @@ from Tree import SegmentTree
 if __name__ == "__main__":
     arr = [3, 8, 7, 5, -10, -9, 55]
     tree = SegmentTree(arr)
+    tree.printTree("minmax")
     tree.printTree()
-    # ans = tree.query(2, 6)
-    # print(ans)
-    # tree.update(2, 10)
-    # tree.printTree()
-    print(tree.rangeSum(2, 5))
--- SegmentTrees/Tree.py
+++ SegmentTrees/Tree.py
@@ -1,10 +1,39 @@
 from Node import Node
 from collections import deque
+
 class SegmentTree:
     def __init__(self, arr: list):
-        self.__root = self.createTree(0, len(arr) - 1, arr)
+        self.__rootForRangeSum = self.createTreeForRangeSum(arr)
+        self.__rootForMinMax = self.createTreeForMinMax(arr)
     
-    def createTree(self, start: int, end: int, arr: list) -> Node:
+    def createTreeForMinMax(self, arr):
+        return self.__createTreeForMinMaxHelper(0, len(arr) - 1, arr)
+    
+    def __createTreeForMinMaxHelper(self, start, end, arr):
+        if start == end:
+            leaf = Node(start, end)
+            leaf.data = (arr[start], arr[end])
+            return leaf
+        mid = start + (end - start) // 2
+        node = Node(start, end)
+
+        node.left = self.__createTreeForMinMaxHelper(start, mid, arr)
+        node.right = self.__createTreeForMinMaxHelper(mid + 1, end, arr)
+
+        node.data = self.__getMinMax(node.left.data, node.right.data)
+
+        return node
+    
+    def __getMinMax(self, num1, num2):
+        return (
+            min(num1[0], num2[0]),
+            max(num1[1], num2[1])
+        )
+    
+    def createTreeForRangeSum(self, arr):
+        return self.__createTreeForRangeSumHelper(0, len(arr) - 1, arr)
+    
+    def __createTreeForRangeSumHelper(self, start: int, end: int, arr: list) -> Node:
         if start > end:
             return None
         if start == end:
@@ -14,15 +43,18 @@ class SegmentTree:
         mid = start + (end - start) // 2
         node = Node(start, end)
         
-        node.left = self.createTree(start, mid, arr)
-        node.right = self.createTree(mid + 1, end, arr)
+        node.left = self.__createTreeForRangeSumHelper(start, mid, arr)
+        node.right = self.__createTreeForRangeSumHelper(mid + 1, end, arr)
 
         node.data = node.left.data + node.right.data
 
         return node
-    
-    def printTree(self):
-        root = self.__root
+    def printTree(self, key=""):
+        if key.lower() == 'minmax':
+            self.__printTreeHelper(self.__rootForMinMax)
+        else:
+            self.__printTreeHelper(self.__rootForRangeSum)
+    def __printTreeHelper(self, root):
         q = deque()
         q.append(root)
 
@@ -38,6 +70,7 @@ class SegmentTree:
                 if node.right is not None:
                     q.append(node.right)
             print()
+    
     def query(self, start, end):
         root = self.__root
         return self.__queryHelper(root, start, end)
@@ -59,6 +92,7 @@ class SegmentTree:
         """
         root = self.__root
         self.__updateHelper(root, index, newValue)
+    
     def __updateHelper(self, root, index, newValue) -> tuple:
         if root.start == index and root.end == index:
             prevValue = root.data
@@ -73,9 +107,11 @@ class SegmentTree:
             right = self.__updateHelper(root.right, index, newValue)
             root.data += -left[0] -right[0] + left[1] + right[1]
             return (left[0] + right[0], left[1] + right[1])
+    
     def rangeSum(self, start: int, end: int) -> int:
         root = self.__root
         return self.__rangeSumHelper(root, start, end)
+    
     def __rangeSumHelper(self, node, start, end):
         if node.start >= start and node.end <= end:
             return node.data
